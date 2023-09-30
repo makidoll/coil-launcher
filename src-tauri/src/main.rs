e8e678a9a22abf16bab8eb14bea1771c69d1c2d8
Maki <mxmcube@gmail.com>
2023-09-30 06:48:27 -0400

    Initial commit

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,23 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use std::env;
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+fn main() {
+    if env::consts::OS == "linux" {
+        // https://github.com/tauri-apps/tauri/issues/5143
+        // https://bugs.webkit.org/show_bug.cgi?id=180739
+        env::set_var("WEBKIT_DISABLE_COMPOSITING_MODE", "1");
+    }
+
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
